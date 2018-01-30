cc853878a00f4cd830309e7ec61780e9c1b7dfc2
Stanimir Hristov <darmenter@outlook.com>
2018-01-30 13:16:50 +0200

    First Release

---
+++ CompleteBinaryTreeFathers.cpp
@@ -0,0 +1,249 @@
+﻿//#include "stdafx.h"
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+#include <ctime>
+#include <algorithm>
+#include <fstream>
+using namespace std;
+
+struct Player2
+{
+	string name;
+	int coefficient;
+	Player2() {
+		name = " ";
+		coefficient = -1;
+	}
+	friend ostream& operator<<(ostream& os, const Player2 &pl) {
+		if (pl.coefficient != -1)
+			os << pl.name ;
+		else os << "-|-";
+		return os;
+	}
+	friend istream& operator>>(istream& is, Player2 & pl) {
+		is >> pl.name >> pl.coefficient;
+		return is;
+	}
+};
+
+template<typename T>
+class TreeFathers      // Complete Binary Тree Fathers List
+{
+	vector<T> info;
+	vector<int> fathers;
+	int numberOfLines() {     // Намира броя на редовете в файл, помощтна за задача 6
+		int num = 0;
+		string line;
+		ifstream myfile("participants.txt");
+		if (myfile.is_open()) {
+			while (!myfile.eof()) {
+				getline(myfile, line);
+				num++;
+			}
+			myfile.close();
+		}
+		return num;
+	}
+public:
+	TreeFathers() {           // Default конструктор
+		fathers.push_back(-2);
+	}
+	TreeFathers(const TreeFathers<T>& tr){           // Copy конструктор
+		for (int i = 0; i< tr.fathers.size(); i++) {
+			info[i] = tr.info[i];
+			fathers[i] = tr.fathers[i];
+		}
+	}
+	TreeFathers& operator=(const TreeFathers& tr){   // Predefined operator =
+		if (this != &tr) {
+			info.clear();
+			father.clear();
+			for (int i = 0; i< tr.fathers.size(); i++) {
+				info[i] = tr.info[i];
+				fathers[i] = tr.fathers[i];
+			}
+		}
+		return *this;
+	}
+	bool empty() const { return fathers[0] == -2; }  // Проверява дали дървото е празно
+	T getRoot() const{    // Връща корена
+		if (empty()) cout << "The tree is empty!\n";
+		else return info[0];
+	}
+	T getFather(const T& child){   // Връща бащата на подаден елемент
+		if (!empty()) {
+			if (child == info[0]) 
+				cout << "The child( " << child << " ) is the root of the tree and has no father!\n";
+			else {
+				for (int i = 1; i < fathers.size(); i++) {
+					if (child == info[i])
+						return info[fathers[i]];
+				}
+				cout << "The child( " << child << " ) has no father and is not in the tree!\n";
+			}
+		}
+		else cout << "The tree is empty, there are no father!\n";
+		return T();
+	}
+	void addNode(const T& elem){    // Добавя елемент на правилната позиция към дървото
+		info.push_back(elem);
+		if (empty()) fathers[0] = -1;
+		else fathers.push_back((fathers.size()-1) / 2);
+	}
+	void fathersIndex(){ // Извежда индексите на бащите
+		for (int i = 0; i < fathers.size(); i++)
+			cout << fathers[i] << "  ";
+		cout << endl;
+	}
+	void printInfo(){  // Извежда елементите в редица 
+		for (int i = 0; i < info.size(); i++)
+			cout << info[i] << '\t';
+		cout << endl;
+	}
+	void print(){  // Извежда дурвото на нива
+		int size = info.size() - 1;
+		int from = 0, to = 0;
+		while (from<size){
+			if (to > size)to = size ;
+			for (int i = from; i <= to; i++)
+				cout << info[i] << "    ";
+			cout << endl;
+			from *= 2; from += 1;
+			to *= 2; to += 2;
+		}
+		cout << endl;
+	}
+	// Задача 5
+	void fillTree(vector<T> leaves){
+		srand(unsigned(time(0)));
+		random_shuffle(leaves.begin(), leaves.end());  // Рамешва вектора от участници
+		int interRoot = leaves.size() - 1;
+		for (size_t i = 0; i < interRoot; i++)
+			addNode(T());
+		for (int i = 0; i < leaves.size(); i++) // Запълва листата на дървото с участниците
+			addNode(leaves[i]);
+	}
+	// Задача 6
+	vector<T> getParticipants() {
+		vector<T> vec;
+		bool* taken = new bool[numberOfLines()]();  // Haмира броя на запълнените редове във файла
+		int takenCount = 0;
+		srand(time(NULL));
+		int n = rand() % numberOfLines();   //  Избира random брои участници
+		while (takenCount < n) {    // Избира random редове от файла
+			int randLine = rand() % numberOfLines();
+			if (!taken[randLine]) {
+				taken[randLine] = true;
+				takenCount++;
+			}
+			else { continue; }
+		}
+		int i = 0;
+		Player2 line;
+		ifstream myfile("participants.txt");
+		if (myfile.is_open()){   
+			while (myfile >> line){     // Запълва vector от random избраните участници от файла
+				if (taken[i])
+					vec.push_back(line);
+				i++;
+			}
+			myfile.close();
+		}
+		return vec;
+	}
+	// Задача 7
+	void tournament(){
+		int index = fathers.size()-1;
+		while (index > 1) {
+			int firstCoef = info[index--].coefficient;
+			int secondCoef = info[index].coefficient;
+			srand(time(NULL));
+			int n = rand() % (firstCoef+secondCoef);  // Избира random число от 0 до сбора от коефициентите на 2-та участника
+			if (n < firstCoef)                       // Ако числото е < от коефициента на 1-вия то той е победител и продължава напред
+				info[fathers[index]] = info[index + 1];
+			else info[fathers[index]] = info[index];
+			index--;
+		}
+	}
+	// Задача 8
+	void findpath(string name) {
+		if (empty()) {
+			cout << "Tree is empty!\n";
+			return;
+		}
+		int i = info.size()-1;
+		while (info[i].name != name && i >= 0) {
+			i--;
+			if (i < 0) {
+				cout << "There no player with that name\n";
+				return;
+			}
+		}
+		while (info[fathers[i]].name == name) {     // Изкарва всички мачове които подаденият участник е спечелил
+			if (name == info[fathers[i] * 2 + 1].name) 
+				cout << name << " - " << info[fathers[i] * 2 + 2] << endl;
+			else 
+				cout << info[fathers[i] * 2 + 1] << " - " << name << endl;
+			i = fathers[i];
+			if (i == 0)break;
+		}
+		if (i == 0) return;
+		if (name == info[fathers[i] * 2 + 1].name)    // Изкарва мача който е загубил или финала на турнира
+			cout << name << " - " << info[fathers[i] * 2 + 2] << endl;
+		else 
+			cout << info[fathers[i] * 2 + 1] << " - " << name << endl;
+		return;
+	}
+	// Задача 9
+	void allTogether(){
+		vector<T> theVec = getParticipants();
+		fillTree(theVec);
+		tournament();
+		cout << "Winner: " << getRoot() << endl;
+		print();
+		string playerIn;
+		cout << "Input Player: ";
+		cin >> playerIn;
+		findpath(playerIn);
+		cout << endl;
+	}
+};
+
+int main(){
+	TreeFathers<Player2> trf1;  // Task 9 (3 times) to show its all random  
+	trf1.allTogether();
+	TreeFathers<Player2> trf2;
+	trf2.allTogether();
+	TreeFathers<Player2> trf3;
+	trf3.allTogether();
+	cout << "Test General (Task 9)                    -- Success !\n";
+
+	TreeFathers<Player2> tr;
+	tr.print();
+
+	Player2 pl1, pl2, pl3;
+	pl1.name = "Mike";
+	pl1.coefficient = 10;
+	pl2.name = "Jerry";
+	pl2.coefficient = 20;
+	pl3.name = "Danni";
+	pl3.coefficient = 30;
+	TreeFathers<Player2> tr1;
+	tr1.addNode(pl1);
+	tr1.addNode(pl2);
+	tr1.addNode(pl3);
+	tr1.print();
+	cout << "Test 1 (AddNode, print)	-- Success !\n";
+	tr1.printInfo();
+	cout << "Test 2 (Vector of elements)	-- Success !\n";
+	tr1.fathersIndex();
+	cout << "Test 3 (Vector of index of fathers)	-- Success !\n";
+
+	cout << tr1.getRoot() << endl;
+	cout << "Test 4 (getRoot)	-- Success !\n";
+
+	if (!tr1.empty()) cout << "Test 5 (empty)	-- Success !\n";
+	return 0;
+}
---
+++ RootCompleteBinaryTree.cpp
@@ -0,0 +1,285 @@
+﻿//#include "stdafx.h"
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+#include <ctime>
+#include <algorithm>
+#include <fstream>
+#include <queue>
+using namespace std;
+
+struct Player3
+{
+	string name;
+	int coefficient;
+
+	Player3() {
+		name = "";
+		coefficient = -1;
+	}
+	friend ostream& operator<<(ostream& os, const Player3 &pl) {
+		if (pl.coefficient != -1)
+			os << pl.name;
+		else os << "|";
+		return os;
+	}
+	bool operator==(Player3 const& p3) {
+		if (name == p3.name) { return true; }
+		return false;
+	}
+	friend istream& operator>>(istream& is, Player3 & pl) {
+		is >> pl.name >> pl.coefficient;
+		return is;
+	}
+};
+
+template<class T>
+class Tree 
+{
+private:
+	T root;
+	Tree<T> *left, *right;
+
+	void CopyTree(Tree<T> const &other) {
+		root = other.root;
+
+		if (other.left) {
+			left = new Tree<T>;
+			GetLeft().CopyTree(other.GetLeft());
+		}
+		if (other.right) {
+			right = new Tree<T>;
+			GetRight().CopyTree(other.GetRight());
+		}
+	}
+public:
+	Tree() {    // Default constructor
+		root = T();
+		left = right = NULL;
+	}
+	~Tree() {    // Destructor
+		if (left) { delete left; }
+		if (right) { delete right; }
+	}
+	Tree(Tree<T> const &other) {   // Copy constructor
+		CopyTree(other);
+	}
+	Tree<T>& operator=(Tree<T> const &other) {  // Predifined operator =
+		if (this != &other) {
+			if (left) { delete left; }
+			if (right) { delete right; }
+			CopyTree(other);
+		}
+		return *this;
+	}
+	Tree(T val) { // Constructor with parameter
+		root = val;
+		left = right = NULL;
+	}
+	T GetRoot() { return root; }						// Returns the root value of the current tree.
+	bool empty() const { return (root == T() || left == NULL); }  // Check if the tree is empty
+	Tree<T>& GetLeft() const { return *left; }			// Returns the left subtree of the current tree.
+	Tree<T>& GetRight() const { return *right; }		// Returns the right subtree of the current tree. 
+	void addNode(T val) {                       // Add element to the tree
+		if (left == NULL) { left = new Tree<T>(val); }
+		else if (right == NULL) { right = new Tree<T>(val); }
+		else {
+			queue<Tree<T>*> children;
+			children.push(left);
+			children.push(right);
+			Tree<T> *temp;
+			while (true) {
+				temp = children.front();
+				if (temp->left) { children.push(temp->left); }
+				else {
+					temp->left = new Tree<T>(val);
+					break;
+				}
+				if (temp->right) { children.push(temp->right); }
+				else {
+					temp->right = new Tree<T>(val);
+					break;
+				}
+				children.pop();
+			}
+		}
+	}
+	void print() const {   
+		if (left) { left->print(); }
+		if (right) { right->print(); }
+		cout << root << " | ";
+	}
+	Tree<T>& getParent(T elem) {  // Return the father of the element
+		if (left == NULL) { return; }
+		if (left->root.name == elem.name) { return *this; }
+		if (right == NULL) { return; }
+		if (right->root.name == elem.name) { return *this; }
+	}
+	// Задача 5
+	void fillTree(vector<T> leaves){
+		srand(unsigned(time(0)));
+		random_shuffle(leaves.begin(), leaves.end()); //shuffle vector
+		int interRoot = leaves.size() - 2;
+		for (int i = 0; i < interRoot; i++)
+			addNode(T());
+		for (int i = 0; i < leaves.size(); i++) //fill the leaves of the tree
+			addNode(leaves[i]);
+	}
+	int numberOfLines() {   // Return the number of the file lines
+		int num = 0;
+		string line;
+		ifstream myfile("participants.txt");
+		if (myfile.is_open()) {
+			while (!myfile.eof()) {
+				getline(myfile, line);
+				num++;
+			}
+			myfile.close();
+		}
+		return num;
+	}
+	// Задача 6
+	vector<T> getParticipants() {
+		bool* taken = new bool[numberOfLines()]();
+		int takenCount = 0;
+		srand(time(NULL));
+		int n = rand() % numberOfLines();
+		while (takenCount < n) {
+			int randLine = rand() % numberOfLines();
+			if (taken[randLine] == false) {
+				taken[randLine] = true;
+				takenCount++;
+			}
+			else { continue; }
+		}
+
+		ifstream myfile("participants.txt");
+		vector<T> vec;
+		if (myfile.is_open()) {
+			int i = 0;
+			Player3 line;
+			while (myfile >> line) {
+				if (taken[i])
+					vec.push_back(line);
+				i++;
+			}
+			myfile.close();
+		}
+		return vec;
+	}
+	// Задача 7
+	void tournament() {
+		if (left == NULL) { return; }
+		else if (right == NULL) {
+			root.name = left->root.name;
+			root.coefficient = left->root.coefficient;
+			return;
+		}
+		left->tournament();
+		right->tournament();
+		srand(time(NULL));
+		int firstCoef = left->root.coefficient;
+		int secondCoef = right->root.coefficient;
+		int random = rand() % (firstCoef + secondCoef);
+		if (random < firstCoef) {
+			root.name = left->root.name;
+			root.coefficient = left->root.coefficient;
+		}
+		else {
+			root.name = right->root.name;
+			root.coefficient = right->root.coefficient;
+		}
+	}
+	// Задача 8
+	void findpath(string name) {
+		if (left == NULL) { return; }
+		else if (left->root.name == name) {
+			left->findpath(name);
+			cout << left->root.name << " - ";
+			cout << right->root.name << "   ";
+		}
+		else if (right == NULL) { return; }
+		else if (right->root.name == name) {
+			right->findpath(name);
+			cout << right->root.name << " - ";
+			cout << left->root.name << "   ";
+		}
+		else {
+			if (left == NULL) { return; }
+			left->findpath(name);
+			if (right == NULL) { return; }
+			right->findpath(name);
+		}
+	}
+	// Задача 9
+	void allTogether(){
+		vector<T> theVec = getParticipants();
+		fillTree(theVec);
+		tournament();
+		print();
+		cout << "\nThe winner is: " << GetRoot() << endl;
+		string name;
+		cout << "Enter player: ";
+		cin >> name;
+		findpath(name);
+		cout << endl;
+	}
+};
+
+int main()
+{
+	Tree<Player3> tr1;    // Test: Task 9 (3 times to show its all random)               => Passed
+	tr1.allTogether();    
+	Tree<Player3> tr2;
+	tr2.allTogether();
+	Tree<Player3> tr3;
+	tr3.allTogether();
+
+	Tree<int> tr;										// Test: Default Constructor	=> Passed
+	cout << !tr.empty() << endl;						// Test: empty()				=> Passed
+	for (int i = 1; i < 25; i++) { tr.addNode(i); }		// Test: addNode(val)			=> Passed
+	tr.print();											// Test: print()				=> Passed
+
+	system("pause");
+
+	// Test: GetLeft(), GetRight(), GetRoot()											=> Passed
+	cout << tr.GetRoot() << '\t' << tr.GetLeft().GetRoot() << '\t' << tr.GetRight().GetRoot() << endl;
+
+	system("pause");
+
+	// Test: Copy Constructor															=> Passed
+	Tree<int> a(tr);
+	a.print();
+	cout << endl;
+
+	system("pause");
+
+	// Test: operator=																	=> Passed
+	Tree<int> c, b;
+	c = b = a;
+	b.print();
+	cout << endl;
+	c.print();
+	cout << endl;
+
+	system("pause");
+	
+	Tree<Player3> scheme;
+	cout << scheme.numberOfLines();						// Test: numberOfLines()		=> Passed
+
+	vector<Player3> theVec = scheme.getParticipants();	// Test: getParticipants()		=> Passed
+	scheme.fillTree(theVec);							// Test: filltree(vector)		=> Passed		
+	scheme.tournament();								// Test: tournament()			=> Passed
+	scheme.print();										
+	cout << endl;
+	cout << "The winner is: " << scheme.GetRoot() << endl;
+
+	string name;
+	cout << "Enter player: ";
+	cin >> name;
+	scheme.findpath(name);								// Test: findpath(name)			=> Passed
+
+	system("pause");
+	return 0;
+}
