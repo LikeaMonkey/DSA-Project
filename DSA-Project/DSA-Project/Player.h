4d2e11240f2059c93eea21945d02cc30d05a19d8
Stanimir Hristov <darmenter@outlook.com>
2018-10-21 22:42:24 +0300

    Added move semantics to Player.h

--- DSA-Project/DSA-Project/Player.h
+++ DSA-Project/DSA-Project/Player.h
@@ -8,9 +8,24 @@ struct Player
 	std::string name;
 	int coefficient;
 
-	Player() { coefficient = -1; }
-	Player(const std::string& _name, int coef) : name(_name), coefficient(coef)
-	{}
+	Player() : coefficient(-1) {}
+	Player(const std::string& _name, int coef) : name(_name), coefficient(coef){}
+	Player(const Player& other) = default;
+	Player& operator=(const Player& other) = default;
+
+	Player(Player&& other) noexcept
+	{
+		name = std::move(other.name);
+		coefficient = other.coefficient;
+	}
+
+	Player& operator=(Player&& other) noexcept
+	{
+		name = std::move(other.name);
+		coefficient = other.coefficient;
+
+		return *this;
+	}
 
 	bool operator==(const Player& pl) const noexcept
 	{
